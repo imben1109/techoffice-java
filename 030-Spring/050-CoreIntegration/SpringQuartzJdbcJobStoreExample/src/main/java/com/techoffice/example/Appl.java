c4066a9839748ef645b2987927350addea4eb614
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2016-11-22 18:01:40 +0800

    update

--- 030-Spring/050-CoreIntegration/SpringQuartzJdbcJobStoreExample/src/main/java/com/techoffice/example/Appl.java
+++ 030-Spring/050-CoreIntegration/SpringQuartzJdbcJobStoreExample/src/main/java/com/techoffice/example/Appl.java
@@ -1,18 +1,47 @@
 package com.techoffice.example;
 
+import java.util.List;
+import java.util.Set;
+
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.impl.matchers.GroupMatcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Appl {
+	
+	@Autowired
+	private Scheduler scheduler;
+	
 	static ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-	public void run(){
+	public void run() throws SchedulerException{
 		System.out.println("Spring Quartz Example");
+		List<String> groups = scheduler.getJobGroupNames();
+		for (String group: groups){
+			Set<JobKey> jobKeySet = scheduler.getJobKeys(GroupMatcher.jobGroupEquals(group));
+			for(JobKey jobKey: jobKeySet){
+				List<Trigger> triggerList = (List<Trigger>) scheduler.getTriggersOfJob(jobKey);
+				System.out.println("Group Id: " + jobKey.getGroup() + " and Job Id: " + jobKey.getName());
+				System.out.println("=================================");
+				for (Trigger trigger: triggerList){
+					System.out.println("Next Fire Time: " + trigger.getNextFireTime());
+				}
+			}
+		}
+		System.out.println("=================END=================");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
 	}
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws SchedulerException{
 		Appl appl = context.getBean(Appl.class);
 		appl.run();
 	}
--- 030-Spring/050-CoreIntegration/SpringQuartzJdbcJobStoreExample/src/main/java/com/techoffice/example/job/SimpleJob.java
+++ 030-Spring/050-CoreIntegration/SpringQuartzJdbcJobStoreExample/src/main/java/com/techoffice/example/job/SimpleJob.java
@@ -1,5 +1,7 @@
 package com.techoffice.example.job;
 
+import java.util.Date;
+
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
@@ -12,7 +14,7 @@ public class SimpleJob extends QuartzJobBean {
 
 	@Override
 	protected void executeInternal(JobExecutionContext ctx) throws JobExecutionException {
-		System.out.println("Run Simple Job");
+		System.out.println(new Date() + ": Run Simple Job");
 	}
 
 }
