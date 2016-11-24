36147a982aa2e3a5970b7921861e37d39659d3fd
Cheng Pan Wai <imben1109@Cheng-Pan-Wais-MacBook-Pro.local>
2016-11-24 22:53:10 +0800

    update

--- 900-Others/050-Cache/010-Ehcache/EhcacheUserManagedCacheExample/src/main/java/com/techoffice/example/Appl.java
+++ 900-Others/050-Cache/010-Ehcache/EhcacheUserManagedCacheExample/src/main/java/com/techoffice/example/Appl.java
@@ -1,33 +1,20 @@
 package com.techoffice.example;
 
-import org.ehcache.Cache;
-import org.ehcache.CacheManager;
-import org.ehcache.config.CacheConfiguration;
-import org.ehcache.config.builders.CacheConfigurationBuilder;
-import org.ehcache.config.builders.CacheManagerBuilder;
-import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.UserManagedCache;
+import org.ehcache.config.builders.UserManagedCacheBuilder;
 
 public class Appl {
 	public static void main(String[] args) {
 
 		// Create Cache Manager 
-		CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder().build();
-		cacheManager.init();
-
-		// Create Cache 
-		ResourcePoolsBuilder resourcePoolsBuilder = ResourcePoolsBuilder.heap(10);
-		CacheConfiguration<Long, String> cacheConfiguration = CacheConfigurationBuilder
-				.newCacheConfigurationBuilder(Long.class, String.class, resourcePoolsBuilder).build();
-		Cache<Long, String> simpleCache = cacheManager.createCache("simpleCache", cacheConfiguration);
+		UserManagedCache<Long, String> userManagedCache = UserManagedCacheBuilder.
+				newUserManagedCacheBuilder(Long.class, String.class).build(false);
+		userManagedCache.init();
 		
 		// Put data into cache
-		simpleCache.put(1L, "Simple Ehcahce");
-		
-		// Get data from cache
-		String value = simpleCache.get(1L); 
-		System.out.println(value);
-		
+		userManagedCache.put(1L, "Simple Ehcahce");
+		System.out.println(userManagedCache.get(1L));
 		// Close the Cache Manager
-		cacheManager.close(); 
+		userManagedCache.close(); 
 	}
 }
