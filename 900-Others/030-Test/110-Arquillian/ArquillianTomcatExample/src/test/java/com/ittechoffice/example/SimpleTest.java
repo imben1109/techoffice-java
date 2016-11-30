3957d29e08b2a18eedbca795c19f8683ed6147aa
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2016-11-30 18:11:38 +0800

    update

--- 900-Others/030-Test/110-Arquillian/ArquillianTomcatExample/src/test/java/com/ittechoffice/example/SimpleTest.java
+++ 900-Others/030-Test/110-Arquillian/ArquillianTomcatExample/src/test/java/com/ittechoffice/example/SimpleTest.java
@@ -1,18 +1,33 @@
 package com.ittechoffice.example;
 
+import java.io.File;
+
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
+@RunAsClient
 public class SimpleTest {
 	
-    @Deployment
+    @Deployment(testable = false)
     public static WebArchive createDeployment() {
     	WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war");
+    	
+    	File[] files = Maven.configureResolver().loadPomFromFile("pom.xml")
+    		.importRuntimeAndTestDependencies().resolve().withTransitivity().asFile();
+        for(File file : files){
+        	war.addAsLibrary(file);
+        }       
+        
+    	war.addClass(TestBean.class);
+    	war.setWebXML(new File("src/test/resources/web.xml"));
+
         return war;
     }
     
