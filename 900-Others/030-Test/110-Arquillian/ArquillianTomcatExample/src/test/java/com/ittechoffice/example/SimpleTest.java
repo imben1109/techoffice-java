0dcee27f0fffc57a67dc5d2e67ae6806d7e21ef8
Cheng Pan Wai <imben1109@Cheng-Pan-Wais-MacBook-Pro.local>
2016-12-01 02:31:31 +0800

    update

--- 900-Others/030-Test/110-Arquillian/ArquillianTomcatExample/src/test/java/com/ittechoffice/example/SimpleTest.java
+++ 900-Others/030-Test/110-Arquillian/ArquillianTomcatExample/src/test/java/com/ittechoffice/example/SimpleTest.java
@@ -3,8 +3,10 @@ package com.ittechoffice.example;
 import java.io.File;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.jsfunit.api.InitialPage;
+import org.jboss.jsfunit.jsfsession.JSFClientSession;
+import org.jboss.jsfunit.jsfsession.JSFServerSession;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
@@ -12,26 +14,29 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
-@RunAsClient
 public class SimpleTest {
 	
-    @Deployment(testable = false)
+    @Deployment
     public static WebArchive createDeployment() {
     	WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war");
     	
     	File[] files = Maven.configureResolver().loadPomFromFile("pom.xml")
     		.importRuntimeAndTestDependencies().resolve().withTransitivity().asFile();
+    	
         for(File file : files){
         	war.addAsLibrary(file);
         }       
         
     	war.addClass(TestBean.class);
+    	war.addAsWebResource(new File("src/test/resources/faces/hello.xhtml"));
+    	war.addAsWebResource(new File("src/test/resources/faces/welcome.xhtml"));
     	war.setWebXML(new File("src/test/resources/web.xml"));
 
         return war;
     }
     
     @Test
-    public void test(){
+    @InitialPage("/hello.xhtml")
+    public void test(JSFClientSession client, JSFServerSession server) throws Exception{
     }
 }
