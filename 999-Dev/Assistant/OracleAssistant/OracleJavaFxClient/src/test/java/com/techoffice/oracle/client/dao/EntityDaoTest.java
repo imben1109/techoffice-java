d3929202babc9d53acfa6fecddbb7822ed6efc73
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2016-12-29 15:05:35 +0800

    update

--- 999-Dev/Assistant/OracleAssistant/OracleJavaFxClient/src/test/java/com/techoffice/oracle/client/dao/EntityDaoTest.java
+++ 999-Dev/Assistant/OracleAssistant/OracleJavaFxClient/src/test/java/com/techoffice/oracle/client/dao/EntityDaoTest.java
@@ -18,9 +18,9 @@ public class EntityDaoTest {
 	
 	@Test
 	public void getParentTableList(){
-		List<EntityTable> results = entityDao.getEntityTableList();
-		for (EntityTable entityTable: results){
-			System.out.println(entityTable.getTableName());
-		}
+//		List<EntityTable> results = entityDao.getEntityTableList();
+//		for (EntityTable entityTable: results){
+//			System.out.println(entityTable.getTableName());
+//		}
 	}
 }
