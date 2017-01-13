a9001ae7e3d3572720f7bd703fc4a5c7992ab0d8
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-01-13 18:41:34 +0800

    update

--- 900-Others/060-Index/010-Lucene/Lucene6Example/src/main/java/com/techoffice/example/Indexer.java
+++ 900-Others/060-Index/010-Lucene/Lucene6Example/src/main/java/com/techoffice/example/Indexer.java
@@ -7,10 +7,13 @@ import java.nio.file.Paths;
 import org.apache.commons.io.FileUtils;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.Field;
 import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
@@ -21,7 +24,8 @@ public class Indexer {
 	
 	public Indexer(String path) throws IOException{
 		indexDirectory = FSDirectory.open(Paths.get(path));
-        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());        
+        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());     
+        config.setOpenMode(OpenMode.CREATE_OR_APPEND);
         indexWriter= new IndexWriter(indexDirectory, config);
 	}
 	
@@ -30,7 +34,10 @@ public class Indexer {
 		String content = FileUtils.readFileToString(file, "UTF-8");
 		document.add(new TextField("path", file.getPath(), Store.YES));
 		document.add(new TextField("content", content, Store.YES));
-		indexWriter.addDocument(document);
+		indexWriter.updateDocument(new Term("id", file.getPath()), document);
+		indexWriter.maybeMerge();
+		indexWriter.commit();
+		
 	}
 	
 	public void close() throws IOException{
