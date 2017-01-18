97ad674e92dab22273c46ddcaeb042040de83327
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-01-18 18:12:42 +0800

    update

--- 030-Spring/070-Boot/SpringBootJspExample/src/main/java/com/techoffice/example/SampleController.java
+++ 030-Spring/070-Boot/SpringBootJspExample/src/main/java/com/techoffice/example/SampleController.java
@@ -1,7 +1,12 @@
-package com.ittechoffice.example;
+package com.techoffice.example;
+
+import java.awt.Desktop;
+import java.net.URI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
