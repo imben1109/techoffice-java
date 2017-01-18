3d485b60e5b82af55252dd0c8482e3c80dd05212
imben1109 <imben1109@gmail.com>
2017-01-19 00:54:51 +0800

    update

---
+++ 999-Dev/StockAnalysis/StockAnalysis-Yahoo/src/test/java/com/techoffice/hkex/stock/service/CsvImportServiceTest.java
@@ -0,0 +1,28 @@
+package com.techoffice.hkex.stock.service;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.techoffice.hkex.stock.dao.StockDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/beans.xml")
+public class CsvImportServiceTest {
+	
+	@Autowired
+	private CsvImportService csvImportService;
+	
+	@Autowired
+	private StockDao stockDao;
+	
+	@Test
+	public void updateStockList() throws IOException{
+		csvImportService.updateStockList();
+		System.out.println(stockDao.list().size());
+	}
+}
