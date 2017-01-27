d261f466990218e124b6a7438a5ca649f5c99d6b
imben1109 <imben1109@gmail.com>
2017-01-27 22:57:04 +0800

    update

--- 030-Spring/080-Batch/SpringBatchJpaPagingItemReaderExample/src/main/java/com/techoffice/example/Appl.java
+++ 030-Spring/080-Batch/SpringBatchJpaPagingItemReaderExample/src/main/java/com/techoffice/example/Appl.java
@@ -1,19 +1,12 @@
 package com.techoffice.example;
 
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.techoffice.example.model.Student;
 
 public class Appl {
 	
