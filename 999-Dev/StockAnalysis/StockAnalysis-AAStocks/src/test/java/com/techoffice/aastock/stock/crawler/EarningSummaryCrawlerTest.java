37440a7b547841ef17ef9162a09387e9c28de175
imben1109 <imben1109@gmail.com>
2017-02-05 22:57:35 +0800

    update

---
+++ 999-Dev/StockAnalysis/StockAnalysis-AAStocks/src/test/java/com/techoffice/aastock/stock/crawler/EarningSummaryCrawlerTest.java
@@ -0,0 +1,42 @@
+package com.techoffice.aastock.stock.crawler;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.xml.sax.SAXException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/beans.xml")
+public class EarningSummaryCrawlerTest {
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private EarningSummaryCrawler earningSummaryCrawler;
+	
+//	@Test
+	public void getYearList() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException{
+		earningSummaryCrawler.getYearList("00010");
+	}
+	
+//	@Test
+	public void getEarningPerShare() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException{
+		earningSummaryCrawler.getEarningPerShare("00010");
+	}
+	
+	@Test
+	public void test() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException{
+		earningSummaryCrawler.getDividendPerShare("00010");
+	}
+	
+	
+}
