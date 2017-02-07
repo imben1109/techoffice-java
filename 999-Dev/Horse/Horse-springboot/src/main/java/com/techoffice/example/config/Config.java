f9bbd3a36e353c4a5bdabfc5553ce021c89813fe
imben1109 <imben1109@gmail.com>
2017-02-07 22:47:26 +0800

    update

--- 999-Dev/Horse/Horse-springboot/src/main/java/com/techoffice/example/config/Config.java
+++ 999-Dev/Horse/Horse-springboot/src/main/java/com/techoffice/example/config/Config.java
@@ -1,4 +1,4 @@
-package com.techoffice.example;
+package com.techoffice.example.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
--- 999-Dev/Horse/Horse-springboot/src/main/java/com/techoffice/example/controller/SampleController.java
+++ 999-Dev/Horse/Horse-springboot/src/main/java/com/techoffice/example/controller/SampleController.java
@@ -1,4 +1,4 @@
-package com.techoffice.example;
+package com.techoffice.example.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
