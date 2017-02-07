c351b952cd310ade0c24e44fa89f5f45d81bdea8
imben1109 <imben1109@gmail.com>
2017-02-07 23:14:45 +0800

    update

--- 999-Dev/Horse/Horse-springboot/src/main/java/com/techoffice/example/SpringBootExampleAppl.java
+++ 999-Dev/Horse/Horse-springboot/src/main/java/com/techoffice/example/SpringBootExampleAppl.java
@@ -1,17 +1,6 @@
 package com.techoffice.example;
 
-import javax.xml.xpath.XPathExpressionException;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.techoffice.example.config.Config;
-import com.techoffice.util.exception.XmlUtilDocumentConversionException;
-
-
 public class SpringBootExampleAppl {
 	
-	public static void main(String[] args) throws XPathExpressionException, XmlUtilDocumentConversionException{
-        ConfigurableApplicationContext context = SpringApplication.run(Config.class);
-	}
+
 }
