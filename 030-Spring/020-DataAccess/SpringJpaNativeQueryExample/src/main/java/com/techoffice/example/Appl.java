bc6687798598904dc01c63dc8e241ee28e2884ee
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-02-28 09:31:33 +0800

    update

--- 030-Spring/020-DataAccess/SpringJpaNativeQueryExample/src/main/java/com/techoffice/example/Appl.java
+++ 030-Spring/020-DataAccess/SpringJpaNativeQueryExample/src/main/java/com/techoffice/example/Appl.java
@@ -2,29 +2,31 @@ package com.techoffice.example;
 
 import java.util.List;
 
-import org.hibernate.Session;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.techoffice.example.dao.StudentDao;
-import com.techoffice.example.model.Student;
-
 @Component
 public class Appl {
 	
-	@Autowired
-	private StudentDao studentDao;
+	@PersistenceContext
+	private EntityManager em;
 	
 	@Transactional
 	public void run(){
-		Student student = new Student();
-		student.setName("Test 1");
-		studentDao.persist(student);
-		List<Student> results = studentDao.findAll();
-		System.out.println("Total Student: " + results.size());
+		Query query ;
+		query = em.createNativeQuery("insert into student values (1, 'Test Student', 'Test Desc')");
+		query.executeUpdate();
+		query = em.createNativeQuery("select * from student");
+		List<Object[]> results = query.getResultList();
+		for (Object[] result: results){
+			System.out.println(result[0].toString() + " " + result[1].toString() + " " + result[2].toString());
+		}
 	}
 	
 	public static void main(String[] args){
