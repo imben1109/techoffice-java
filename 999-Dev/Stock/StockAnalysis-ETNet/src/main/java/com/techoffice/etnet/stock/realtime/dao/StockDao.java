3a8fdc7edbe6f2ae66d68e9ff2f0e5416f977507
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-02-28 13:51:05 +0800

    update

--- 999-Dev/Stock/StockAnalysis-ETNet/src/main/java/com/techoffice/etnet/stock/realtime/dao/StockDao.java
+++ 999-Dev/Stock/StockAnalysis-ETNet/src/main/java/com/techoffice/etnet/stock/realtime/dao/StockDao.java
@@ -1,13 +1,15 @@
 package com.techoffice.etnet.stock.realtime.dao;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StockDao {
 	
-	@Autowired
-	private SessionFactory sessionFactory;
+	@PersistenceContext
+	private EntityManager em;
+	
 	
 }
