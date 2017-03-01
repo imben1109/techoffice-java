1fc0b30027be7203ece65d0f43f87f143e74b993
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-03-01 11:39:49 +0800

    update

---
+++ 999-Dev/Word/WordQuery/src/main/java/com/tehoffice/example/poi/excel/SimpleExcelReaderExample.java
@@ -0,0 +1,29 @@
+package com.tehoffice.example.poi.excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class SimpleExcelReaderExample {
+	
+	public static void main(String[] args) throws IOException{
+		FileInputStream file = new FileInputStream(new File("SimpleExcelReaderExample.xls"));
+		HSSFWorkbook workbook = new HSSFWorkbook(file);
+		HSSFSheet sheet = workbook.getSheetAt(0);
+		Iterator<Row> rowIterator = sheet.iterator();
+		while(rowIterator.hasNext()) {
+			Row row = rowIterator.next();
+			Iterator<Cell> cellIterator = row.cellIterator();
+			while(cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
+				System.out.println(cell.getStringCellValue());
+			}
+		}
+	}
+}
