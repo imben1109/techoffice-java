bca35603a04a2f420c8c939e2663ce02e42b7fad
imben1109 <imben1109@gmail.com>
2017-03-05 12:35:52 +0800

    update

--- 999-Dev/Horse/Horse-springboot/src/main/java/com/techoffice/example/controller/SampleController.java
+++ 999-Dev/Horse/Horse-springboot/src/main/java/com/techoffice/example/controller/SampleController.java
@@ -1,5 +1,9 @@
 package com.techoffice.example.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.xml.xpath.XPathExpressionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.techoffice.jc.horse.dto.CurrentOdd;
 import com.techoffice.jc.horse.service.CurrentOddService;
 import com.techoffice.util.exception.XmlUtilDocumentConversionException;
 import com.techoffice.util.exception.XmlUtilXpathNotUniqueException;
@@ -24,8 +29,17 @@ public class SampleController {
 
 	@RequestMapping("/test")
 	@ResponseBody
-	public String test() throws XPathExpressionException, XmlUtilDocumentConversionException, XmlUtilXpathNotUniqueException{
-		currentOddService.getCurrentOddMap();
-		return "";
+	public Map<String, List<CurrentOdd>> test() throws XPathExpressionException, XmlUtilDocumentConversionException, XmlUtilXpathNotUniqueException{
+		Map<String, List<CurrentOdd>> map = currentOddService.getCurrentOddMap();
+		return map;
+	}
+	
+	@RequestMapping("/map")
+	@ResponseBody
+	public Map<String, String> getMap(){
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("Test1", "Testing Value 1");
+		map.put("Test2", "Testing Value 2");
+		return map;
 	}
 }
