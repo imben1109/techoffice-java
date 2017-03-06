26bb685fe681092d018759dd64bb082eabca72c3
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-03-06 10:56:55 +0800

    update

--- 999-Dev/Word/WordQuery/src/main/java/com/tehoffice/word/Appl.java
+++ 999-Dev/Word/WordQuery/src/main/java/com/tehoffice/word/Appl.java
@@ -11,12 +11,12 @@ import com.tehoffice.word.helper.TableRowListHelper;
 public class Appl {
 	
 	public static void main(String[] args) throws IOException{
-		WordDocument document = new WordDocument(new File("SimpleWordWriterExample.docx"));
+		WordDocument document = new WordDocument(new File("\\\\dvservk1\\EMS\\50-Testing\\90-Test Plan\\40-Regression Test\\Test Plan (PRD v2.1.0)\\EMS-Test Scenario and Cases_Batch Jobs (Schedule Jobs)_v.2.1.0.docx"));
 		List<List<XWPFTableRow>> results = TableRowListHelper.splitRowList(document.getTable(0).list(), 0, "Function ID & Name:");
 		results.remove(0);
 		for (List<XWPFTableRow> result: results){
 			result.remove(1);
-			System.out.println(TableRowListHelper.toText(result, "\n", "\n\n\n"));
+			System.out.println(TableRowListHelper.toText(result, "\r\n", "\r\n\r\n"));
 		}
 	}
 	
