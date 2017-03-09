b8d2a54d005b834346c1d01672c3155672d8994e
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-03-09 17:40:21 +0800

    Update

---
+++ 900-Others/250-Image/020-OpenCv/CustomCascadeExample/src/main/java/com/techoffice/example/Appl.java
@@ -0,0 +1,50 @@
+package com.techoffice.example;
+
+import java.io.File;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+public class Appl {
+	
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+	
+	public static Mat getGrayMat(File imageFile){
+	    Mat mat = Imgcodecs.imread(imageFile.getAbsolutePath(), Imgcodecs.IMREAD_COLOR);
+	    if (mat.empty()){
+	    	throw new RuntimeException("Cannot read image file: " + imageFile.getAbsolutePath());
+	    }
+		Mat grayMat = new Mat();
+	    Imgproc.cvtColor(mat, grayMat, Imgproc.COLOR_BGR2GRAY);
+	    return grayMat;
+	}
+	
+	public static void main(String[] args){
+		// Image 1
+		Mat mat = getGrayMat(new File("sunglasses_0.png"));
+		File cascadeXml = new File("cascade.xml");
+		
+		CascadeClassifier faceCascade = new CascadeClassifier(cascadeXml.getAbsolutePath());
+		
+		// Detect face
+		MatOfRect detector = new MatOfRect();
+		faceCascade.detectMultiScale(mat, detector);								
+		for (Rect rect : detector.toArray()) {
+			Imgproc.rectangle(mat, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+					new Scalar(0, 255, 0), 3);
+		}
+
+		// Write Image with detected face and eye
+		Imgcodecs.imwrite("face.png", mat);
+
+	}
+}
