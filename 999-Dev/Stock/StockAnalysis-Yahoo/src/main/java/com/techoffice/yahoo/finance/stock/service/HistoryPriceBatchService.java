da8ef4b4c1d3b5feb0653a9d061577c7cf60dd4e
imben1109 <imben1109@gmail.com>
2017-03-16 21:25:58 +0800

    update

--- 999-Dev/Stock/StockAnalysis-Yahoo/src/main/java/com/techoffice/yahoo/finance/stock/service/HistoryPriceBatchService.java
+++ 999-Dev/Stock/StockAnalysis-Yahoo/src/main/java/com/techoffice/yahoo/finance/stock/service/HistoryPriceBatchService.java
@@ -1,15 +1,13 @@
 package com.techoffice.yahoo.finance.stock.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -26,7 +24,8 @@ public class HistoryPriceBatchService {
 
 	public void run() {
 		try{
-			JobExecution execution = simpleJobLauncher.run(updateHistoryPriceJob, new JobParameters());
+			JobParameters paramerter = new JobParametersBuilder().addDate("date", new Date()).toJobParameters();
+			JobExecution execution = simpleJobLauncher.run(updateHistoryPriceJob, paramerter);
 			List<Throwable> exceptionList = execution.getAllFailureExceptions();
 			for (Throwable exception: exceptionList){
 				System.out.println(exception.getMessage());
