d03105f8c25d8865f9e9b7969d87945fec23bb42
imben1109 <imben1109@gmail.com>
2017-05-29 22:53:23 +0800

    update oracle pojo generator

--- 999-Dev/Assistant/Oracle/OracleAssistant/OracleJavaModelGenerator/src/main/java/com/techoffice/oracle/javamodel/Appl.java
+++ 999-Dev/Assistant/Oracle/OracleAssistant/OracleJavaModelGenerator/src/main/java/com/techoffice/oracle/javamodel/Appl.java
@@ -2,12 +2,16 @@ package com.techoffice.oracle.javamodel;
 
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.sun.codemodel.JClassAlreadyExistsException;
+import com.techoffice.oracle.javamodel.config.OracleConfig;
+import com.techoffice.oracle.javamodel.service.PojoService;
 
 
 @Component
@@ -15,12 +19,17 @@ public class Appl {
 
 	static ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 	
+	@Autowired
+	private PojoService pojoService;
+	
 	@Transactional()
-	public void run() throws URISyntaxException, IOException{
-
+	public void run() throws JClassAlreadyExistsException, IOException{
+		String tableName = OracleConfig.getGeneratorTable();
+		String packageName = OracleConfig.getGeneratorPackage();
+		pojoService.generate(packageName, tableName);
 	}
 	
-	public static void main(String[] args) throws URISyntaxException, IOException{
+	public static void main(String[] args) throws IOException, JClassAlreadyExistsException{
 		Appl appl = context.getBean(Appl.class);
 		appl.run();
 	}
