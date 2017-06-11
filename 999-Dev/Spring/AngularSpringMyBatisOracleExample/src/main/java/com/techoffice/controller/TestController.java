964696578b690b6ac99b8778015bce364b2e1245
imben1109 <imben1109@gmail.com>
2017-06-11 08:36:00 +0800

    update

---
+++ 999-Dev/Spring/AngularSpringMyBatisOracleExample/src/main/java/com/techoffice/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.techoffice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.techoffice.model.Test;
+import com.techoffice.service.TestService;
+
+@RequestMapping("/Test")
+@Controller
+public class TestController {
+
+	@Autowired
+	private TestService testService;
+	
+	@RequestMapping("/search")
+	@ResponseBody
+	public List<Test> search(){
+		return testService.search();
+	}
+}
---
+++ 999-Dev/Spring/AngularSpringMyBatisOracleExample/src/main/java/com/techoffice/model/Test.java
@@ -0,0 +1,33 @@
+package com.techoffice.model;
+
+public class Test {
+	private int id;
+	private int age;
+	private String address;
+	private String phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
