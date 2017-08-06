e70cdb70e0c0c99ae515f473be55182d2829b5c8
imben1109 <imben1109@gmail.com>
2017-08-06 23:05:53 +0800

    update

--- 030-Spring/030-Web/SpringMvcFileDownloadExample/src/main/java/com/techoffice/example/controller/SimpleController.java
+++ 030-Spring/030-Web/SpringMvcFileDownloadExample/src/main/java/com/techoffice/example/controller/SimpleController.java
@@ -1,16 +1,12 @@
 package com.techoffice.example.controller;
 
-import java.io.File;
 import java.io.IOException;
-
-import javax.servlet.http.Part;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SimpleController {
@@ -26,21 +22,10 @@ public class SimpleController {
 	 * @return view name 
 	 * @throws IOException 
 	 */
-	@RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
-	public String fileUpload(@RequestParam("file") MultipartFile file, ModelMap map) throws IOException{
-		
-		// Create Temporary File for storing uploaded file.
-		File tmpFile = File.createTempFile(file.getOriginalFilename(), null);
-		tmpFile.deleteOnExit();
-		file.transferTo(tmpFile);
-		System.out.println("Temporary File Name: " + tmpFile.getName());
-		System.out.println("Temporary File Path: " + tmpFile.getPath());
-		
-		map.put("uploadedFilename", file.getOriginalFilename());
-		map.put("tmpFileName", tmpFile.getName());
-		map.put("tmpFilePath", tmpFile.getPath());
-		
-		return "fileUpload";
+	@RequestMapping(value = "/fileDownload", method = RequestMethod.GET)
+	@ResponseBody
+	public byte[] fileDownload() throws IOException{
+		return null;
 	}
 	
 
--- 030-Spring/030-Web/SpringMvcJacksonExample/src/main/java/com/techoffice/example/SimpleController.java
+++ 030-Spring/030-Web/SpringMvcJacksonExample/src/main/java/com/techoffice/example/SimpleController.java
@@ -23,4 +23,5 @@ public class SimpleController {
 		return user;	
 	}
 	
+	
 }
