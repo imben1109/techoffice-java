1bd52316bf327d1987315b2d3b7fd7d6ea4c7d0b
imben1109 <imben1109@gmail.com>
2017-08-08 01:36:38 +0800

    update

--- 999-Dev/Stock/StockAnalysis-AAStocks/src/main/java/com/techoffice/aastock/stock/model/Job.java
+++ 999-Dev/Stock/StockAnalysis-AAStocks/src/main/java/com/techoffice/aastock/stock/model/Job.java
@@ -14,5 +14,19 @@ public class Job {
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="JOB_SEQ")
 	private int id;
 	private String tableName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTableName() {
+		return tableName;
+	}
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+	
 	
 }
--- 999-Dev/Stock/StockAnalysis-AAStocks/src/test/java/com/techoffice/aastock/stock/dao/JobDaoTest.java
+++ 999-Dev/Stock/StockAnalysis-AAStocks/src/test/java/com/techoffice/aastock/stock/dao/JobDaoTest.java
@@ -1,8 +1,11 @@
 package com.techoffice.aastock.stock.dao;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +15,7 @@ import com.techoffice.aastock.stock.model.Job;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/beans.xml")
 @Transactional
+@Rollback(false)
 public class JobDaoTest {
 
 	@Autowired
@@ -21,5 +25,14 @@ public class JobDaoTest {
 	public void add(){
 		Job job = new Job();
 		jobDao.add(job);
+	}
+	
+	@Test
+	public void listAll(){
+		List<Job> jobs = jobDao.list();
+		System.out.println(jobs.size());
+		for (Job job: jobs){
+			System.out.println(job.getId());
+		}
 	}
 }
