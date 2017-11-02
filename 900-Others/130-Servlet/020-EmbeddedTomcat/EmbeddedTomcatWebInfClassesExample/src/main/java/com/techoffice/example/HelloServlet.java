664c0e0afcb402efe9cc90e22f15c3d69a495906
Pan Wai CHENG <panwaicheng@dbs.com>
2017-11-02 18:18:12 +0800

    update

--- 900-Others/130-Servlet/020-EmbeddedTomcat/EmbeddedTomcatWebInfClassesExample/src/main/java/com/techoffice/example/HelloServlet.java
+++ 900-Others/130-Servlet/020-EmbeddedTomcat/EmbeddedTomcatWebInfClassesExample/src/main/java/com/techoffice/example/HelloServlet.java
@@ -11,20 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
     name = "MyServlet",
-    urlPatterns = {"/hello"}
+    urlPatterns = {"/"}
 )
 public class HelloServlet extends HttpServlet{
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
+        out.write("hello techoffice".getBytes());
         out.flush();
         out.close();
     }
