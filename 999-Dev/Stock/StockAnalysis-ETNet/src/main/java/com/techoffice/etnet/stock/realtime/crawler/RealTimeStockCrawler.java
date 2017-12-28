a28390241da64d16b73d7c3e888217b31205dca7
imben1109 <imben1109@gmail.com>
2017-12-28 08:23:50 +0800

    update

--- 999-Dev/Stock/StockAnalysis-ETNet/src/main/java/com/techoffice/etnet/stock/realtime/crawler/RealTimeStockCrawler.java
+++ 999-Dev/Stock/StockAnalysis-ETNet/src/main/java/com/techoffice/etnet/stock/realtime/crawler/RealTimeStockCrawler.java
@@ -3,6 +3,7 @@ package com.techoffice.etnet.stock.realtime.crawler;
 import org.springframework.stereotype.Component;
 
 import com.techoffice.util.WebDriverUtil;
+import com.techoffice.util.XmlUtil;
 
 /**
  * 
@@ -16,8 +17,14 @@ public class RealTimeStockCrawler {
 	public static final String URL = "http://www.etnet.com.hk/www/tc/stocks/realtime/quote_super.php?code=";
 	
 	public String retrieveXmlByCode(String code) {
-		String xml = WebDriverUtil.getXml(URL+code);
+		String xml = WebDriverUtil.getXml(URL + code);
         return xml;
 	}
 	
+	public String getCurrentPrice(String code){
+		final String xpath = "/html/body/div/div[5]/table[1]/tbody/tr[2]/td[1]/ul/li[2]";
+		String xml = this.retrieveXmlByCode(code);
+		String currentPrice = XmlUtil.getXpathText(xml, xpath);
+		return currentPrice;
+	}
 }
