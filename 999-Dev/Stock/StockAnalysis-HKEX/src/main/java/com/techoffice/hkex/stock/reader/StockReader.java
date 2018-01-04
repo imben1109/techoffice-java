452aef6f98c7326ee6505613a272d6f42f4a421f
Pan Wai CHENG <panwaicheng@dbs.com>
2018-01-04 19:19:08 +0800

    update

--- 999-Dev/Stock/StockAnalysis-HKEX/src/main/java/com/techoffice/hkex/stock/reader/StockReader.java
+++ 999-Dev/Stock/StockAnalysis-HKEX/src/main/java/com/techoffice/hkex/stock/reader/StockReader.java
@@ -1,5 +1,24 @@
 package com.techoffice.hkex.stock.reader;
 
-public class StockReader {
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+
+import com.techoffice.hkex.stock.model.Stock;
+import com.techoffice.reader.base.BaseExcelReader;
+
+public class StockReader extends BaseExcelReader<List<Stock>> {
+
+	@Override
+	public InputStream getConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Stock> getBean() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
---
+++ 999-Dev/Stock/StockAnalysis-HKEX/src/main/java/com/techoffice/util/exception/BaseExcelReaderException.java
@@ -0,0 +1,10 @@
+package com.techoffice.util.exception;
+
+public class BaseExcelReaderException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public BaseExcelReaderException(Throwable t){
+		super(t);
+	}
+}
