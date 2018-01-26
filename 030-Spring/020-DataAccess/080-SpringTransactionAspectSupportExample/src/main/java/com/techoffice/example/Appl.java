2bc9d1ab9c00ef5db69178c2f7a5aa6e0a083ed8
Pan Wai CHENG <panwaicheng@dbs.com>
2018-01-26 09:46:43 +0800

    update

---
+++ 030-Spring/020-DataAccess/080-SpringTransactionAspectSupportExample/src/main/java/com/techoffice/example/Appl.java
@@ -0,0 +1,37 @@
+package com.techoffice.example;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+
+@Component
+public class Appl {
+
+	static ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+	
+	@Autowired
+	private TableDao tableDao;
+
+	
+	public Appl(){
+		System.out.println("Tech Office Spring Data Access Example");
+	}
+	
+	@Transactional()
+	public void run(){
+		tableDao.createTable();
+		tableDao.insertData();
+		tableDao.insertData2();
+		TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+		tableDao.count();
+	}
+	
+	public static void main(String[] args){
+		Appl appl = context.getBean(Appl.class);
+		appl.run();
+	}
+}
