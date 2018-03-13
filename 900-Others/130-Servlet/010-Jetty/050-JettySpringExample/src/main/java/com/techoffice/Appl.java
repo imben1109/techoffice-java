318152356e78558be331e7a2cc2c68f3a28e55f6
ben <ben@techoffice.com>
2018-03-13 22:52:37 +0800

    update

---
+++ 900-Others/130-Servlet/010-Jetty/050-JettySpringExample/src/main/java/com/techoffice/Appl.java
@@ -0,0 +1,39 @@
+package com.techoffice;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class Appl {
+
+	public static void main(String[] args) throws Exception{
+		
+        Server server = new Server(8080);
+        
+        ServletContextHandler servletContextHandler = new ServletContextHandler();
+        server.setHandler(servletContextHandler);
+
+        ServletHandler servletHandler = new ServletHandler();
+        ServletHolder servletHolder = new ServletHolder();
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+        servletHolder.setServlet(dispatcherServlet);
+        servletHolder.setInitParameter("contextConfigLocation", "classpath:/webapp/WEB-INF/beans.xml");
+        servletHandler.addServletWithMapping(servletHolder, "/");
+        
+        servletContextHandler.setServletHandler(servletHandler);
+        servletContextHandler.setBaseResource(Resource.newClassPathResource("/webapp"));
+
+        server.start();
+        server.join();
+        server.join();
+	}
+	
+}
