64cb48e0e84f369794d78fab304c7c1db1671a9a
ben <ben@techoffice.com>
2018-05-12 00:43:42 +0800

    update

--- 999-Dev/DatabaseGenerator/src/main/java/com/techoffice/h2/dao/ConstraintsDao.java
+++ 999-Dev/DatabaseGenerator/src/main/java/com/techoffice/h2/dao/ConstraintsDao.java
@@ -3,10 +3,11 @@ package com.techoffice.h2.dao;
 import java.util.List;
 
 import com.techoffice.h2.model.Constraints;
+import com.techoffice.h2.util.DaoUtil;
 
 public class ConstraintsDao {
 
 	public static List<Constraints> getConstraintsList(String tableName){
-		return null;
+		return DaoUtil.list(Constraints.class, "SELECT * FROM INFORMATION_SCHEMA.CONSTRAINTS WHERE TABLE_NAME ='"+tableName+"'");
 	}
 }
