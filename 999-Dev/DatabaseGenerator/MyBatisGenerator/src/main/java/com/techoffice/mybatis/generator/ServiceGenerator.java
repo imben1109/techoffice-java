44b8570bccf9d3ba6d200410dfb9ab3f64fee502
ben <ben@techoffice.com>
2018-05-14 01:36:01 +0800

    update

--- 999-Dev/DatabaseGenerator/MyBatisGenerator/src/main/java/com/techoffice/mybatis/generator/ServiceGenerator.java
+++ 999-Dev/DatabaseGenerator/MyBatisGenerator/src/main/java/com/techoffice/mybatis/generator/ServiceGenerator.java
@@ -1,12 +1,21 @@
 package com.techoffice.mybatis.generator;
 
+import com.techoffice.database.dao.EntityDao;
 import com.techoffice.database.model.Entity;
+import com.techoffice.database.registry.EntityDaoRegistry;
 import com.techoffice.freemarker.util.FreemakerUtil;
+import com.techoffice.h2.dao.H2EntityDao;
 
 public class ServiceGenerator {
 
 	public static String generate(Entity entity){
 		return FreemakerUtil.generate(ServiceGenerator.class, entity);
 	}
-	
+
+	public static void main(String[] args){
+		EntityDao entityDao = EntityDaoRegistry.getEntityDao(H2EntityDao.class);
+		Entity entity = entityDao.getEntity("TEST");
+		String content = ServiceGenerator.generate(entity);
+		System.out.println(content);
+	}
 }
