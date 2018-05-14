dd3a28109f2c4b896ab2e82601179995a145663e
Pan Wai CHENG <panwaicheng@dbs.com>
2018-05-14 15:31:44 +0800

    udpate

--- 999-Dev/DatabaseGenerator/MyBatisGenerator/src/test/java/com/techoffice/test/h2/TableGeneratorTest.java
+++ 999-Dev/DatabaseGenerator/MyBatisGenerator/src/test/java/com/techoffice/test/h2/TableGeneratorTest.java
@@ -2,14 +2,17 @@ package com.techoffice.test.h2;
 
 import org.junit.Test;
 
+import com.techoffice.database.model.Entity;
 import com.techoffice.mybatis.generator.TableGenerator;
+import com.techoffice.mybatis.util.MockUtil;
 import com.techoffice.test.h2.base.BaseH2GeneratorTest;
 
 public class TableGeneratorTest extends BaseH2GeneratorTest<TableGenerator> {
 
 	@Test
 	public void generate(){
-		String content = generate("TEST");
+		Entity entity = MockUtil.getMockEntity();
+		String content = getTemplateGenerator().generate(entity);
 		System.out.println(content);
 	}
 }
