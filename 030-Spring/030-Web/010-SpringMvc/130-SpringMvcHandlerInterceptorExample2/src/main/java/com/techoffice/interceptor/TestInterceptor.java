154bb2438baaa6d2c519d327d65b739422da1fbc
ben <ben@techoffice.com>
2018-12-29 00:41:04 +0800

    update

--- 030-Spring/030-Web/010-SpringMvc/130-SpringMvcHandlerInterceptorExample2/src/main/java/com/techoffice/interceptor/TestInterceptor.java
+++ 030-Spring/030-Web/010-SpringMvc/130-SpringMvcHandlerInterceptorExample2/src/main/java/com/techoffice/interceptor/TestInterceptor.java
@@ -1,11 +1,17 @@
 package com.techoffice.interceptor;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestInterceptor implements HandlerInterceptor{
 
@@ -13,6 +19,10 @@ public class TestInterceptor implements HandlerInterceptor{
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		String body = IOUtils.toString(request.getReader());
+		if (!StringUtils.isEmpty(body)){
+			ObjectMapper objectMapper = new ObjectMapper();
+			Map<String, Object> map = objectMapper.readValue(body, new TypeReference<Map<String, Object>>(){});			
+		}
 		System.out.println(body);
 		return true;
 	}
