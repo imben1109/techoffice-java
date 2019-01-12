646a0e9a1210f2285be025e3bb408e95dd93c169
ben <ben@techoffice.com>
2019-01-12 14:01:40 +0800

    update

---
+++ 030-Spring/070-Boot/030-Security/020-CustomConfigExample/src/main/java/com/techoffice/Appl.java
@@ -0,0 +1,20 @@
+package com.techoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Appl {
+	
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+	
+	public static void main(String[] args){
+        SpringApplication.run(Appl.class, args);
+	}
+}
