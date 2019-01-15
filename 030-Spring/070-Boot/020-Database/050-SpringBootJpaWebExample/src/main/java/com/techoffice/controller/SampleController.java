3950416a10233310d5ca6f986d97c8d942d3da98
ben <ben@techoffice.com>
2019-01-15 23:34:08 +0800

    update

---
+++ 030-Spring/070-Boot/020-Database/050-SpringBootJpaWebExample/src/main/java/com/techoffice/controller/SampleController.java
@@ -0,0 +1,17 @@
+package com.techoffice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SampleController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "welcome";
+	}
+
+
+
+
+}
