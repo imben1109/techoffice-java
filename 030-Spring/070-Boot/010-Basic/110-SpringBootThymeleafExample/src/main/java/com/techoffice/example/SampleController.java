ef7dc3510f5e855f36dad65d2377d0ea8f5656e4
ben <ben@techoffice.com>
2019-03-27 00:19:29 +0800

    update

---
+++ 030-Spring/070-Boot/010-Basic/110-SpringBootThymeleafExample/src/main/java/com/techoffice/example/SampleController.java
@@ -0,0 +1,21 @@
+package com.techoffice.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    
+	@RequestMapping("/")
+	public String home() {
+        return "welcome";
+    }
+	
+    public static void main(String[] args) throws Exception {
+    	SpringApplication.run(SampleController.class, args);
+    }
+
+}
