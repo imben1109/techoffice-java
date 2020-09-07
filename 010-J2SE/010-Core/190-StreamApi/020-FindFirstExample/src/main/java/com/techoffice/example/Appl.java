cf2e59b36168763ec94191af918509a9e1704797
ben <imben1109@gmail.com>
2020-09-07 17:29:12 +0800

    update

--- 010-J2SE/010-Core/190-StreamApi/020-FindFirstExample/src/main/java/com/techoffice/example/Appl.java
+++ 010-J2SE/010-Core/190-StreamApi/020-FindFirstExample/src/main/java/com/techoffice/example/Appl.java
@@ -10,6 +10,7 @@ public class Appl {
         List<TestModel1> testModel1List = new ArrayList<>();
         TestModel1 testModel11 = new TestModel1();
         TestModel1 testModel12 = new TestModel1();
+        testModel12.setName("Testing");
         List<TestModel2> testModel2List = new ArrayList<>();
         TestModel2 testModel2 = new TestModel2();
         testModel2.setName("testing");
@@ -18,10 +19,7 @@ public class Appl {
         testModel1List.add(testModel11);
         testModel1List.add(testModel12);
 
-        boolean result = testModel1List.stream().flatMap(i ->
-                        i.getTestModel2List() != null ?
-                i.getTestModel2List().stream() : null
-        ).anyMatch(i-> i.getName().equals("testing"));
+        long result = testModel1List.stream().filter(i -> "Testing".equals(i.getName())).count();
         System.out.println(result);
     }
 
--- 900-Others/390-Cyptograhic/010-pgp/src/main/java/com/techoffice/example/Signer.java
+++ 900-Others/390-Cyptograhic/010-pgp/src/main/java/com/techoffice/example/Signer.java
@@ -1,44 +1,102 @@
 package com.techoffice.example;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.bcpg.BCPGOutputStream;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.*;
 import org.bouncycastle.openpgp.operator.PBESecretKeyDecryptor;
+import org.bouncycastle.openpgp.operator.PGPContentSignerBuilder;
 import org.bouncycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator;
 import org.bouncycastle.openpgp.operator.jcajce.JcaPGPContentSignerBuilder;
 import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyDecryptorBuilder;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.util.Date;
+import java.util.Iterator;
 
 @Slf4j
 public class Signer {
 
-    public static void main(String[] args){
-        PGPSecretKey                secretKey = getSecretKey("");
-        PGPPrivateKey               pgpPrivKey = getPrivateKey(secretKey);
-        PGPSignatureGenerator       sGen = new PGPSignatureGenerator(new JcaPGPContentSignerBuilder(pgpSec.getPublicKey().getAlgorithm(), PGPUtil.SHA1).setProvider("BC"));
+    public static void main(String[] args) throws IOException, PGPException {
+        Security.addProvider(new BouncyCastleProvider());
+
+        String singedMessage = signMessage("testing");
+        log.info("Singed Message: {}", singedMessage);
 
     }
 
-    public static PGPSecretKey getSecretKey(String secretKeyFile, long keyId) throws IOException, PGPException {
-        InputStream inputStream = new FileInputStream(new File(secretKeyFile));
-        PGPSecretKeyRingCollection secretKeyRingCollection = new PGPSecretKeyRingCollection(PGPUtil.getDecoderStream(inputStream), new JcaKeyFingerprintCalculator());
-        log.info("Secret Key Collection Size: {}", secretKeyRingCollection.size());
-        PGPSecretKey pgpSecretKey = secretKeyRingCollection.getSecretKey(keyId);
-        if (pgpSecretKey == null){
-            log.info("cannot find secret key with public key id: {}", keyId);
-            return null;
+    public static String signMessage(String message) throws IOException, PGPException {
+        byte[] messageCharArray = message.getBytes();
+
+        PGPSecretKey secretKey = getSecretKey("secretKey.key");
+        PGPPrivateKey pgpPrivKey = getPrivateKey(secretKey, "password");
+
+        PGPContentSignerBuilder signerBuilder = new JcaPGPContentSignerBuilder(
+                secretKey.getPublicKey().getAlgorithm(),
+                PGPUtil.SHA1).setProvider("BC");
+
+        PGPSignatureGenerator signatureGenerator = new PGPSignatureGenerator(signerBuilder);
+        signatureGenerator.init(PGPSignature.BINARY_DOCUMENT, pgpPrivKey);
+
+        Iterator userIds = secretKey.getPublicKey().getUserIDs();
+        if (userIds.hasNext()) {
+            PGPSignatureSubpacketGenerator spGen = new PGPSignatureSubpacketGenerator();
+            spGen.setSignerUserID(false, (String) userIds.next());
+            signatureGenerator.setHashedSubpackets(spGen.generate());
         }
-        return pgpSecretKey;
+
+        PGPCompressedDataGenerator compressedDataGenerator = new PGPCompressedDataGenerator(
+                PGPCompressedData.ZLIB);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        BCPGOutputStream bcpgOutputStream = new BCPGOutputStream(compressedDataGenerator.open(out));
+
+        signatureGenerator.generateOnePassVersion(false).encode(bcpgOutputStream);
+        PGPLiteralDataGenerator literalDataGernateor = new PGPLiteralDataGenerator();
+
+        OutputStream laterOutStream = literalDataGernateor.open(bcpgOutputStream, PGPLiteralData.BINARY,
+                PGPLiteralData.CONSOLE, messageCharArray.length, new Date());
+
+        for (byte c : messageCharArray) {
+            laterOutStream.write(c);
+            signatureGenerator.update(c);
+        }
+
+        laterOutStream.close();
+        literalDataGernateor.close();
+
+        signatureGenerator.generate().encode(bcpgOutputStream);
+
+        compressedDataGenerator.close();
+        return out.toString();
     }
 
-    public static PGPPrivateKey getPrivateKey(PGPSecretKey pgpSecretKey) throws PGPException {
+    public static PGPSecretKey getSecretKey(String secretKeyFile) throws IOException, PGPException {
+        InputStream inputStream = new FileInputStream(new File(secretKeyFile));
+        PGPSecretKeyRingCollection secretKeyRingCollection = new PGPSecretKeyRingCollection(
+                PGPUtil.getDecoderStream(inputStream), new JcaKeyFingerprintCalculator());
+        log.info("Secret Key Collection Size: {}", secretKeyRingCollection.size());
+        Iterator<PGPSecretKeyRing> keyRingIterator = secretKeyRingCollection.getKeyRings();
+        while (keyRingIterator.hasNext()){
+            PGPSecretKeyRing pgpSecretKeyRing = keyRingIterator.next();
+            Iterator<PGPSecretKey> secretKeyIterator = pgpSecretKeyRing.getSecretKeys();
+            while (secretKeyIterator.hasNext()){
+                PGPSecretKey secretKey = secretKeyIterator.next();
+                if (secretKey.isSigningKey()){
+                    return secretKey;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static PGPPrivateKey getPrivateKey(PGPSecretKey pgpSecretKey, String password) throws PGPException {
         JcePBESecretKeyDecryptorBuilder secretKeyDecryptorBuilder = new JcePBESecretKeyDecryptorBuilder();
         secretKeyDecryptorBuilder.setProvider(BouncyCastleProvider.PROVIDER_NAME);
-        PBESecretKeyDecryptor secretKeyDecryptor = secretKeyDecryptorBuilder.build("password".toCharArray());
+        PBESecretKeyDecryptor secretKeyDecryptor = secretKeyDecryptorBuilder.build(password.toCharArray());
         return pgpSecretKey.extractPrivateKey(secretKeyDecryptor);
     }
 }
